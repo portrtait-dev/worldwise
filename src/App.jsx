58ed98629e1a740bfae2b3608e1d25e630cc9c2a
type sh,t <olzhas.dossaibek.dev@gmail.com>
2024-11-21 20:42:01 +0500

    206. Implementing Main Pages and Routes - first 3 Routes/pages defined - in this lecture, use React Router for the first time to create main pages of this application - next, linking between the pages to transition
    Desired behavior:
    1. For 3 different URLs, open 3 different pages (though without navigation yet)
    2. For any non-existing URL, open <PageNotFound />
    
    Underlying implementation:
    1. Create 3 simple pages (and a new folder for them), install react-router-dom via npm, use React Router (BrowserRouter, Routes, Route), connect each Route to a page (using path and element)
    2. Create new simple Component, use path='*'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-function App() {
-  const x = 23;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
-  return <div>Selam</div>;
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

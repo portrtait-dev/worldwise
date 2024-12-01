23cd5d93e0f39757ac125fa96622d03948da07da
type sh,t <olzhas.dossaibek.dev@gmail.com>
2024-12-01 15:34:17 +0500

    p.2 of 2 - 217. Reading and Setting a Query String URL is updated with lat and lng derived lat and lng from URL change URL everywhere with setSearchParams() demonstrates the shareable feature of URLs (because State is in URL) URL is like a global variable now accessible everywhere
    in this lecture, the second way of storing state in URL - Query String
    next, no comment from Jonas
    
    Desired behavior:
    1. Read Query String from URL and show it in UI
    2. Update URL via click in UI
    Underlying implementation:
    1.1. Destructure prop, improve Link (‘to’ property)
    1.2. Access info from URL in other page - Map Component, via useSearchParams()
    1.3. Do the same for the other Component - City Component
    2. Update URL params on click - onClick, setSearchParams() → thus, URL params get updated and all UI gets updated and we can also share this URL and everybody will have the same UI

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,6 +13,9 @@ function Map() {
       <h2>
         Position: {lat}, {lng}
       </h2>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change position
+      </button>
     </div>
   );
 }

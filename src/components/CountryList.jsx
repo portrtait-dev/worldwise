9b2dd8c2daf0bb5401ea11e0ade57ddf457ee3dd
type sh,t <olzhas.dossaibek.dev@gmail.com>
2024-12-05 19:17:56 +0500

    228. Consuming the CitiesContext
    - improve CitiesContext - add useCities() Hook
    CityList and CountryList use our new Hook (instead of Props)
    - we now have refactored our code - removed all logic from the App.jsx (and thus made it clearer, cleaner, removed clutter)
    
    - in this lecture, build the custom Hook to consume CitiesContext everywhere we need
    - next, no comment from Jonah
    
    Desired behavior:
    - Fix other pages from the last lecture to work well - pages which required Props
    
    Underlying implementation:
    1. Create Custom Hook (useCities) - useContext, CitiesContext, throw new Error, named export
    2. Replace Props in the Component with destructuring of the useCities() - in 2 Components: CountryList, CityList

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -3,8 +3,11 @@ import Message from "./Message";
 
 import styles from "./CountryList.module.css";
 import CountryItem from "./CountryItem";
+import { useCities } from "../contexts/CitiesContext";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)

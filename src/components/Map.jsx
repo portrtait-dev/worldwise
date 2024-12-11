29246a84a8cf62c779899b78868562fa7027433b
type sh,t <olzhas.dossaibek.dev@gmail.com>
2024-12-11 18:50:14 +0500

    232. Interacting With the Map
    - describes the problem - Leaflet does not update its map center when data is updated - need to do it manually
    - describes interim problem - Leaflet changes map center to default coordinates in between the cities
    - map now does not jump to default coords - coordinates are synchronized
    - opens on click to the map
    - change map center on click + pass the lat and lng to URL
    
    - in this lecture, set the map to current city + attach event handlers
    - next, no comment from Jonah
    
    Desired behavior:
    1.1. When we change cities, return map to a default position (which is in via the ChangeCenter Component). Problem 1 - map is not reactive, we should make it reactive within the leaflet library, with the use of its internal Components
    1.2. When we change cities, remember the last city and stay there in other non-city pages
    2. On click to map, open the form and center map on the clicked area
    
    Underlying implementation:
    1.1. Fix problem 1: create custom Component (ChangeCenter) which uses Leaflet’s internal Component (useMap), setView(), position as a Prop, return null, use this Component in return part
    1.2. new useEffect, setMapPosition, if clause, update ChangeCenter
    2. Create new Component (DetectClick), use Leaflet’s useMapEvents, navigate(), /form + lat + lng, use DetectClick in MapContainer

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,37 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-  const navigate = useNavigate();
-
   const { cities } = useCities();
 
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
-  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const [mapPosition, setMapPosition] = useState([30, 0]);
 
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if ((mapLat, mapLng)) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        // center={[mapLat, mapLng]}
         zoom={13}
         scrollWheelZoom={true}
         className={styles.map}
@@ -40,7 +51,9 @@ function Map() {
           </Marker>
         ))}
 
-        <ChangeCenter position={[mapLat || 40, mapLng || 0]} />
+        <ChangeCenter position={mapPosition} />
+
+        <DetectClick />
       </MapContainer>
     </div>
   );
@@ -50,6 +63,14 @@ function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
   return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
 }
 
 export default Map;
